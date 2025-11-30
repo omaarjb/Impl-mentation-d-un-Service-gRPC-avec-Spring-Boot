3e7a2efca4ef6a0376366b77d634f2e627cf5f83
Omar Jabbour <128647309+omaarjb@users.noreply.github.com>
2025-11-30 14:52:02 +0100

    done

---
+++ src/test/java/ma/projet/grpc/Tp18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.projet.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
